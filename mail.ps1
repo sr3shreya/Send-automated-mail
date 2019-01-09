817f40855e722fcfbbc6fdb88043af693849accc
Shreya Srivastava <shreya.srivastava963@gmail.com>
2019-01-09 16:48:14 +0530

    Add files via upload

---
+++ mail.ps1
@@ -0,0 +1,12 @@
+﻿$Outlook = New-Object -ComObject Outlook.Application 
+#create Outlook MailItem named Mail using CreateItem() method 
+$Mail = $Outlook.CreateItem(0) 
+
+#add properties as desired 
+$Mail.To = "shreya_s@hcl.com" 
+$Mail.Subject = "Production Checkout" 
+$Mail.Body = "testing" 
+#send message 
+$Mail.Send() 
+#quit and cleanup 
+$Outlook.Quit()
